5c8475faf2088b19acd16d2ace1c3edf89abeea2
andreasavignano02 <112469730+andreasavignano02@users.noreply.github.com>
2022-10-20 19:01:59 +0200

    fix-sem-artist

--- assets/scripts/artist.js
+++ assets/scripts/artist.js
@@ -1,25 +1,31 @@
 async function lista() {
+  // 1 parte
   let queryString2 = new URLSearchParams(window.location.search);
   let idArtist = queryString2.get("id");
-  let cambiamento = await fetch(
-    `https://striveschool-api.herokuapp.com/api/deezer/search?q=${idArtist}`
-  );
+  let cambiamento = await fetch(`https://striveschool-api.herokuapp.com/api/deezer/artist/${idArtist}`);
   let convertito = await cambiamento.json();
-  let convertimentoData = convertito.data;
-  for (let i = 0; i < convertimentoData.length; i++) {
-    document.getElementById("list").innerHTML += `<div id="list-item">
+  // 2 parte
+    document.querySelector(".title-item").innerHTML = convertito.name
+  // 3 parte
+  let listaCanzoni = convertito.tracklist
+  let convertLista = await fetch(listaCanzoni)
+  let convertLista2 = await convertLista.json();
+  let convertLista3 = convertLista2.data
+  // 4 parte
+  for (let i = 0; i < convertLista3.lenght ; i++) {
+    document.querySelector("#list").innerHTML += 
+      `<div id="list-item">
         <div class="allineamento">
-
         <div class="number-item">${i + 1}</div>
-        <img class="img-album" src="${convertimentoData[i].album.cover_small}">
+        <img class="img-album" src="${convertLista3[i].contributors.picture_small}">
         <div class="contenitor-list">
-        ${convertimentoData[i].title}
+        ${convertLista3[i].title}
         </div>
         <div class="visualize">
-            ${convertimentoData[i].rank}
+            ${convertLista3[i].rank}
         </div>
         <div class="duration">
-        ${convertimentoData[i].duration}
+        ${convertLista3[i].duration}
         </div>
         </div>
         <div>`;
